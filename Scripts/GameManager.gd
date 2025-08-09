7671808f5372a16cefc8df4de5dcc89fb0bc343a
Lucas Thomesse <thomesse.lucas@outlook.fr>
2025-08-09 17:16:31 +0200

    Adding a function to generate a world without the castle at the center.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,11 +1,17 @@
 extends Node
 
 @export var seed_value : String
+@export var is_castle_centered : bool
+@export var max_world_map_range : float = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if seed_value != "" and seed_value != null : WorldMap.set_seed(seed_value.hash()) 
+	if seed_value != "" and seed_value != null : WorldMap.set_seed(seed_value.hash())
 	WorldMap.initialise_parameters($"../World")
-	WorldMap.start_gen_world_around_castle(2)
+	if is_castle_centered:
+		WorldMap.start_gen_world_around_castle(max_world_map_range)
+	else:
+		WorldMap.start_gen_world(max_world_map_range)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scripts/WorldMap.gd
+++ Scripts/WorldMap.gd
@@ -11,25 +11,37 @@ func initialise_parameters(origin:Node3D) -> void:
 	var castle_tile_scene : PackedScene = load("res://Scenes/Tiles/castle_tile.tscn")
 	world_origin = origin
 	castle_tile = castle_tile_scene.instantiate()
+ 
+func start_gen_world_around_castle(max_range:float) -> void:
 	world_origin.add_child(castle_tile)
 	castle_tile.position = Vector3(0,0,0)
 	map_tiles[position_in_world_origin(castle_tile.global_position)] = castle_tile
- 
-func start_gen_world_around_castle(max_range:float) -> void:
-	gen_world_around_tile(castle_tile, max_range)
+	gen_world_around_tile(castle_tile, castle_tile, max_range)
 
-func gen_world_around_tile(active_node:Node3D, max_range:float) -> void:
+func start_gen_world(max_range:float) -> void:
+	var starter_tile:Tile = pick_random_world_tile()
+	world_origin.add_child(starter_tile)
+	starter_tile.position = Vector3(0,0,0)
+	map_tiles[position_in_world_origin(starter_tile.global_position)] = starter_tile
+	gen_world_around_tile(starter_tile, starter_tile, max_range)
+	world_origin.add_child(castle_tile)
+	var random_tile:Tile = get_random_tile_from_map()
+	castle_tile.position = random_tile.position
+	map_tiles[castle_tile.position] = castle_tile
+	world_origin.remove_child(random_tile)
+
+func gen_world_around_tile(active_node:Node3D, start_tile:Tile, max_range:float) -> void:
 	var neighbours : Array = active_node.get_children().slice(0,6)
 	for node:Node3D in neighbours:
 		var snapped_global : Vector3 = snapped(node.global_position, Vector3(0.001, 0, 0.001))
 		var node_game_world_pos : Vector3 = position_in_world_origin(snapped_global)
-		var dist : float = node_game_world_pos.distance_to(castle_tile.position)
+		var dist : float = node_game_world_pos.distance_to(start_tile.position)
 		if dist <= max_range and not node_game_world_pos in map_tiles:
 			var tile : Tile = pick_random_world_tile()
 			world_origin.add_child(tile)
 			tile.global_position = snapped_global
 			map_tiles[node_game_world_pos] = tile
-			gen_world_around_tile(tile, max_range)
+			gen_world_around_tile(tile, start_tile, max_range)
 
 func pick_random_world_tile() -> Tile:
 	var random_value : int = RNG.randi_range(1,101)
@@ -48,3 +60,6 @@ func is_in_castle_radius(world_position:Vector3) -> bool:
 
 func set_seed(seed) -> void:
 	RNG.seed = seed
+
+func get_random_tile_from_map() -> Tile:
+	return map_tiles[map_tiles.keys()[RNG.randi_range(0, map_tiles.size())]]
